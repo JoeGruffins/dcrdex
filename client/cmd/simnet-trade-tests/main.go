e1b796945ad0aac6ea853f669a33ebd92d455eba
JoeGruff <joegruffins@gmail.com>
2022-10-17 15:34:00 +0900

    client/tests: Fix simnet tests.
    Fix numerous balance calculating issues. Run most tests twice,
    alternating the maker and taker between clients. Add all, except, and
    runonce commands. Cause requests to fail using communications rather
    than altering match data.

--- client/cmd/simnet-trade-tests/main.go
+++ client/cmd/simnet-trade-tests/main.go
@@ -27,10 +27,19 @@ func parseWalletType(t string) (core.SimWalletType, error) {
 }
 
 func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("SUCCESS!")
+	os.Exit(0)
+}
+
+func run() error {
 	var baseSymbol, quoteSymbol, base1Node, quote1Node, base2Node, quote2Node, regAsset string
-	var base1SPV, quote1SPV, base2SPV, quote2SPV, debug, trace, list bool
+	var base1SPV, quote1SPV, base2SPV, quote2SPV, debug, trace, list, all, runOnce bool
 	var base1Type, quote1Type, base2Type, quote2Type string
-	var tests flagArray
+	var tests, except flagArray
 	flag.Var(&tests, "t", "the test(s) to run. multiple --t flags OK")
 	flag.StringVar(&baseSymbol, "base", "dcr", "the bot program to run")
 	flag.StringVar(&quoteSymbol, "quote", "btc", "the bot program to run")
@@ -50,30 +59,29 @@ func main() {
 	flag.BoolVar(&list, "list", false, "list available tests")
 	flag.BoolVar(&debug, "debug", false, "log at logging level debug")
 	flag.BoolVar(&trace, "trace", false, "log at logging level trace")
+	flag.BoolVar(&all, "all", false, "run all tests for the asset")
+	flag.Var(&except, "except", "can only be used with the \"all\" flag and removes unwanted tests. multiple --except flags OK")
+	flag.BoolVar(&runOnce, "runonce", false, "some tests will run twice to swap maker/taker. Set to only run once. default is false")
 	flag.Parse()
 
 	if list {
 		for _, s := range core.SimTests() {
 			fmt.Println(s)
 		}
-		return
+		return nil
 	}
 
 	if base1SPV {
-		fmt.Fprintln(os.Stderr, "base1spv is removed - use base1type=spv instead")
-		os.Exit(1)
+		return errors.New("base1spv is removed - use base1type=spv instead")
 	}
 	if quote1SPV {
-		fmt.Fprintln(os.Stderr, "quote1spv is removed - use quote1type=spv instead")
-		os.Exit(1)
+		return errors.New("quote1spv is removed - use quote1type=spv instead")
 	}
 	if base2SPV {
-		fmt.Fprintln(os.Stderr, "base2spv is removed - use base2type=spv instead")
-		os.Exit(1)
+		return errors.New("base2spv is removed - use base2type=spv instead")
 	}
 	if quote2SPV {
-		fmt.Fprintln(os.Stderr, "quote2spv is removed - use quote2type=spv instead")
-		os.Exit(1)
+		return errors.New("quote2spv is removed - use quote2type=spv instead")
 	}
 
 	logLevel := dex.LevelInfo
@@ -84,8 +92,31 @@ func main() {
 		logLevel = dex.LevelDebug
 	}
 
+	// Assume any trailing arguments are test names.
+	tests = append(tests, flag.Args()...)
+
+	// Check that all test names are valid.
+	allTests := core.SimTests()
+	var containsAll bool
 	if len(tests) == 0 {
 		tests = []string{"success"}
+	} else {
+		for _, t := range tests {
+			if t == "all" {
+				containsAll = true
+				break
+			}
+			var found bool
+			for _, at := range allTests {
+				if at == t {
+					found = true
+					break
+				}
+			}
+			if !found {
+				return fmt.Errorf("test %s is unknown. Possible tests are %s", t, allTests)
+			}
+		}
 	}
 
 	if regAsset == "" {
@@ -94,26 +125,36 @@ func main() {
 
 	b1wt, err := parseWalletType(base1Type)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "invalid base1 wallet type %q", base1Type)
-		os.Exit(1)
+		return fmt.Errorf("invalid base1 wallet type %q", base1Type)
 	}
 	q1wt, err := parseWalletType(quote1Type)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "invalid quote1 wallet type %q", quote1Type)
-		os.Exit(1)
+		return fmt.Errorf("invalid quote1 wallet type %q", quote1Type)
 	}
 	b2wt, err := parseWalletType(base2Type)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "invalid base2 wallet type %q", base2Type)
-		os.Exit(1)
+		return fmt.Errorf("invalid base2 wallet type %q", base2Type)
 	}
 	q2wt, err := parseWalletType(quote2Type)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "invalid quote2 wallet type %q", quote2Type)
-		os.Exit(1)
+		return fmt.Errorf("invalid quote2 wallet type %q", quote2Type)
 	}
 
-	err = core.RunSimulationTest(&core.SimulationConfig{
+	if all || containsAll {
+		tests = allTests
+		if len(except) > 0 {
+			for i := len(tests) - 1; i >= 0; i-- {
+				for _, ex := range except {
+					if tests[i] == ex {
+						tests = append(tests[:i], tests[i+1:]...)
+						break
+					}
+				}
+			}
+		}
+	}
+
+	return core.RunSimulationTest(&core.SimulationConfig{
 		BaseSymbol:        baseSymbol,
 		QuoteSymbol:       quoteSymbol,
 		RegistrationAsset: regAsset,
@@ -129,14 +170,10 @@ func main() {
 			BaseNode:        base2Node,
 			QuoteNode:       quote2Node,
 		},
-		Tests:  tests,
-		Logger: dex.StdOutLogger("T", logLevel),
+		Tests:   tests,
+		Logger:  dex.StdOutLogger("T", logLevel),
+		RunOnce: runOnce,
 	})
-	if err != nil {
-		fmt.Println("ERROR:", err)
-	} else {
-		fmt.Println("SUCCESS!")
-	}
 }
 
 type flagArray []string
